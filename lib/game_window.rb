e2cb6c375b7de32c342c6fbaec7e79f3c631e33f
justin <justinschueller2@gmail.com>
2017-04-12 18:27:03 +0200

    Ajout de sons et optimisation

--- lib/game_window.rb
+++ lib/game_window.rb
@@ -3,7 +3,6 @@ class Poke
 WINDOW_X = 1024
 attr_reader :type
   def initialize(type)
-    #@type = type
     @image = Gosu::Image.new('images/pika.png')
 
 # La vitesse de deplacement des ruby est variable
@@ -37,18 +36,15 @@ attr_reader :score
     @background_sprite = Gosu::Image.new(self, 'images/pokemon.jpg', true)
     @sacha_sprite = Gosu::Image.new(self, 'images/sacha.png', true)
     @enemy_sprite = Gosu::Image.new(self, 'images/jessie.png', true)
-    @flag_sprite = Gosu::Image.new(self, 'images/flag.png', true)
     @font = Gosu::Font.new(self, Gosu::default_font_name, 30)
-    @flag = {x: WINDOW_X - SPRITE_SIZE, y: WINDOW_Y - SPRITE_SIZE}
-    @music = Gosu::Song.new(self, "musics/koala.wav")
+    @music = Gosu::Song.new(self, 'musics/poke1.wav')
     @items = []
     @score = 0
     reset
-
   end
 
   def update
-
+    #Ajoute des pokemons
     unless @items.size >= 5
       r = rand
       if r < 0.035
@@ -58,6 +54,9 @@ attr_reader :score
 
     @items.each(&:update)
     @items.reject! {|item| item.y > WINDOW_Y }
+
+    #Evenements quand les poke et les ennemis sont touchés ou collectés
+    #Et lors de l'appui sur les touches claviers
     collect_pika(@items)
     collect_enemy(@enemies)
      @player[:x] += @speed if button_down?(Gosu::Button::KbRight)
@@ -72,6 +71,7 @@ attr_reader :score
      handle_enemies
      handle_quit
 
+    #Passage au niveau ou un reset du jeu en fonction du score
    case @score
    when 150
      @score = 0
@@ -82,6 +82,7 @@ attr_reader :score
    end
   end
 
+  #Dessin des ennemis, du player, de la carte
   def draw
     @font.draw("Level #{@enemies.length}", WINDOW_X - 100, 10, 3, 1.0, 1.0, Gosu::Color::BLACK)
     @font.draw("Score: #{@score}", WINDOW_X - 1000, 10, 3, 1.0, 1.0, Gosu::Color::BLACK)
@@ -101,8 +102,8 @@ attr_reader :score
     @vertical_velocity = 100
   end
 
+  #Fonction de saut
   def handle_jump
-
     gravity = 1.3
     ground_level = 768
     @player[:y] -= @vertical_velocity
@@ -121,25 +122,26 @@ attr_reader :score
     end
   end
 
-
+    # A chaque pika collecté, ce dernier disparait, le score est incrémenté et un son est émis
     def collect_pika(items)
       items.reject! do |item|
         (item.x - @player[:x]).abs < 50 and (item.y - @player[:y]).abs < 50
-        if Gosu.distance(@player[:x], @player[:y], item.x, item.y) < 35
+        if Gosu.distance(@player[:x], @player[:y], item.x, item.y) < 50
           @score += 10
-          #@beep.play
+          @beep= Gosu::Sample.new('musics/piece.wav')
+          @beep.play
           true
         else
           false
         end
       end
     end
+    # A chaque pika collecté,  le score est decrémenté mias l'ennemi ne disparait pas
     def collect_enemy(enemies)
       enemies.reject do |enemy|
         (enemy[:x] - @player[:x]).abs < 50 and (enemy[:y] - @player[:y]).abs < 50
-        if Gosu.distance(@player[:x], @player[:y], enemy[:x], enemy[:y]) < 35
-          @score -= 50
-          #@beep.play
+        if Gosu.distance(@player[:x], @player[:y], enemy[:x], enemy[:y]) < 50
+          @score -= 5
           true
         else
           false
@@ -150,16 +152,17 @@ attr_reader :score
   private
 
 
-
+  #Reinitialise le score, les ennemis, la vitesse et lance la musique
   def reset
     @high_score = 0
     @enemies = []
     @speed = 3
     #@music.stop
-    #@music.play
+    @music.play
     reinit
   end
 
+  #Commencer le niveau ou passer ou niveau suivant en ajoutant un ennemi
   def reinit
     @speed += 1
     @player = {x: 0, y: 768}
@@ -167,6 +170,7 @@ attr_reader :score
     high_score
   end
 
+  #Ecrire un fichier avec le score du joueur
   def high_score
     unless File.exist?('hiscore')
       File.new('hiscore', 'w')
@@ -174,12 +178,6 @@ attr_reader :score
     @new_high_score = [@enemies.count, File.read('hiscore').to_i].max
     File.write('hiscore', @new_high_score)
   end
-
-  def collision?(a, b)
-    (a[:x] - b[:x]).abs < SPRITE_SIZE / 2 &&
-    (a[:y] - b[:y]).abs < SPRITE_SIZE / 2
-  end
-
 
   def random_mouvement
     (rand(3) - 1)
@@ -195,12 +193,14 @@ attr_reader :score
     end
   end
 
+  #Quitte le jeu
   def handle_quit
     if button_down? Gosu::KbEscape
       close
     end
   end
 
+  #Fais déplacer les ennemis de façon aléatoire
   def handle_enemies
     @enemies = @enemies.map do |enemy|
       enemy[:timer] ||= 0
@@ -216,9 +216,7 @@ attr_reader :score
       new_enemy[:y] += new_enemy[:result_y] * @speed
       new_enemy[:x] = normalize(new_enemy[:x], WINDOW_X - SPRITE_SIZE)
       new_enemy[:y] = normalize(new_enemy[:y], WINDOW_Y - SPRITE_SIZE)
-      unless collision?(new_enemy, @flag)
-        enemy = new_enemy
-      end
+      enemy = new_enemy
       enemy
     end
   end
